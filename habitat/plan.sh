b9684f9023bba41a4b85be563925216235ce092f
Sridharan <DSRIDHA5@ford.com>
2018-07-19 16:09:09 -0400

    Plan Update

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,1 @@
-pkg_name=habitatize
-pkg_origin=learn-chef
-pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.1.0"
-pkg_deps=( core/imagemagick )
+pkg_name=habitatize pkg_origin=dsridharan pkg_scaffolding="core/scaffolding-ruby" pkg_version="0.1.0" pkg_deps=( core/imagemagick )
